67555c880df2a3a445bed0bd9de6244d65cf4540
Leevi12 <leevi.sallantaus@gmail.com>
2021-03-30 13:18:53 +0300

    Ei ymmärrä yhtään...

--- Tehtävä 3/script.js
+++ Tehtävä 3/script.js
@@ -1,10 +1,1 @@
-function tulostus () {
-
-  var i;
-  var text = "";
-  for (i = 0; i > 1; i--) {
-    text += "Parilliset numerot ovat: " +  i + "<br>";
-  }
-  document.getElementById("numerosi").value = text;
-}
-// Ei toimi vielä
+// Ei vaan ymmärrä nyt yhtään
