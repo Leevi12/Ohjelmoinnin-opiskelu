ce4016456b1f8a52329e6e637f604e8b97fcf4e1
Leevi12 <leevi.sallantaus@gmail.com>
2021-03-30 11:53:22 +0300

    Ei toimi vielä/30.3.

---
+++ Tehtävä 3/script.js
@@ -0,0 +1,10 @@
+function tulostus () {
+
+  var i;
+  var text = "";
+  for (i = 0; i > 1; i--) {
+    text += "Parilliset numerot ovat: " +  i + "<br>";
+  }
+  document.getElementById("numerosi").value = text;
+}
+// Ei toimi vielä
