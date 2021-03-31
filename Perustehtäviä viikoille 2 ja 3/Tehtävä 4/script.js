9bf099dd6e2c2bd80cf2ca41db7602322481610a
Leevi12 <leevi.sallantaus@gmail.com>
2021-03-31 12:34:04 +0300

    Tehtävät valmiit

---
+++ Perustehtäviä viikoille 2 ja 3/Tehtävä 4/script.js
@@ -0,0 +1,138 @@
+function laske()
+{
+  var pisteet = 0;
+  var sanasi = document.getElementById('scrabble').value;
+  for(var i = 0; i < sanasi.length; i++)
+  {
+    switch (sanasi[i]) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'n':
+      case 's':
+      case 't':
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'N':
+      case 'S':
+      case 'T':
+        pisteet++;
+        break;
+      case 'o':
+      case 'ä':
+      case 'k':
+      case 'l':
+      case 'O':
+      case 'Ä':
+      case 'K':
+      case 'L':
+        pisteet+=2;
+        break;
+      case 'u':
+      case 'm':
+      case 'U':
+      case 'M':
+        pisteet+=3;
+        break;
+      case 'y':
+      case 'h':
+      case 'j':
+      case 'p':
+      case 'r':
+      case 'v':
+      case 'Y':
+      case 'H':
+      case 'J':
+      case 'P':
+      case 'R':
+      case 'V':
+        pisteet+=4;
+        break;
+      case 'ö':
+      case 'd':
+      case 'Ö':
+      case 'D':
+        pisteet+=7;
+        break;
+      case 'b':
+      case 'f':
+      case 'g':
+      case 'B':
+      case 'F':
+      case 'G':
+        pisteet+=8;
+        break;
+      case 'c':
+      case 'C':
+        pisteet+=10;
+        break;
+      default:
+        pisteet+=12;
+    }
+    document.getElementById('sanaSeppo').innerHTML = "<p>Sanan " + sanasi + " pisteet ovat: " + pisteet + "</p>";
+  }
+}
+  function arvonta()
+  {
+    var lottonum = [];
+    var temp;
+    for(var lotto = 0; lotto < 7; lotto++)
+    {
+      temp = Math.floor(Math.random()*40)+1;
+      lottonum[lotto] = temp;
+    }
+    lottonum.sort();
+
+    document.getElementById('lotto').innerHTML = '<p>'+lottonum+'</p>';
+  }
+
+
+  function muuta()
+{
+  var jono = [];
+  jono = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+  var taulu = '<table border="1">';
+  for(var j = 0; j < jono.length; j++)
+  {
+    taulu += '<tr>';
+    for(var k = 0; k < jono[j].length; k++)
+    {
+      taulu += '<td>' + jono[j][k] + '</td>';
+    }
+    taulu += '</tr>';
+  }
+  taulu += '</table>';
+  document.getElementById('taulukko').innerHTML = taulu;
+}
+
+
+function arvonta()
+{
+  var pakka = [''];
+  var maat = ['&#9828;', '&#9827;', '&#9826;', '&#9825;'];
+  var numerot = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+  laskuri = 0;
+  for(m = 0; m < maat.length; m++)
+  {
+    for(n = 0; n < numerot.length; n++)
+    {
+      if(m == maat.length-1 && n == numerot.length-1)
+      {
+        pakka[laskuri] = maat[m] + numerot[n];
+      }
+      else {
+        pakka[laskuri] = maat[m]+numerot[n];
+        laskuri++;
+      }
+    }
+  }
+  var kortit = [];
+  for(l = 0; l < 5; l++)
+  {
+      temp = pakka[Math.floor(Math.random()*52)];
+      kortit[l] = temp;
+  }
+  document.getElementById('kortit').innerHTML = '<p>'+kortit+'</p>';
+}
